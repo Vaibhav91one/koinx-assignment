de39c5190e675d3b7e9edf493b2657178e8f3a1b
Vaibhav91one <vtking03@gmail.com>
2024-03-07 16:19:43 +0530

    feat: Sentiment Section setup

--- src/components/sections/Sentiment.tsx
+++ src/components/sections/Sentiment.tsx
@@ -1,10 +1,127 @@
 import React from 'react'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Info } from 'lucide-react'
+import { SENTIMENT_ITEMS } from '@/constants'
+import { TrendingUp, Newspaper, BarChart } from 'lucide-react';
+import { Progress } from '../ui/progress'
 
+const icons = [
+  <TrendingUp className='text-white rounded-full m-4' />, <Newspaper className='text-white rounded-full m-4' />, <BarChart className='text-white rounded-full m-4' />
+]
+
+type CardProps = {
+  icon: any;
+  bg: string;
+}
 type Props = {}
+
+const SentimentCard = ({ icon, bg }: CardProps) => {
+  return (
+    <>
+      <div className='flex items-start justify-start gap-8'>
+        <div className={`rounded-full ${bg}`}>
+          {icon}
+        </div>
+        <div >
+          <p className='font-semibold'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et magnam sint in
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et magnam sint in non delectus quisquam dolorem, modi facilis! Libero sint alias cupiditate explicabo ipsum eaque nobis laudantium veritatis quis incidunt.
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
 
 const Sentiment = (props: Props) => {
   return (
-    <div>Sentiment</div>
+    <>
+      <section className='max-container flex flex-col items-start justify-start md:gap-12 lg:py-14'>
+        <div className='flex flex-col gap-5 items-center justify-start'>
+          <h1 className='sectionTitle'>
+            Sentiment
+          </h1>
+          <h3 className='flex items-center justify-start gap-2 text-md font-semibold text-gray-600'>
+            Key Events <Info className='bg-gray-600 rounded-2xl text-white ' />
+          </h3>
+        </div>
+
+        <Carousel className="w-full">
+          <CarouselContent className="-ml-1">
+            {SENTIMENT_ITEMS.map((item, index) => (
+              <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-2/3">
+                <div className="p-1">
+                  <SentimentCard icon={icons[index]} bg={item.bg} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext className="mr-16 drop-shadow-2xl" />
+        </Carousel>
+
+        <div>
+          <h3 className='flex items-center justify-start gap-2 text-md font-semibold text-gray-600'>
+            Analyst Estimates <Info className='bg-gray-600 rounded-2xl text-white ' />
+          </h3>
+        </div>
+
+        <div className='flex justify-start items-start gap-24'>
+          <div className='flex justify-center items-center p-5 m-5'>
+            <h2 className='text-green-500 text-5xl font-semibold'>
+              76%
+            </h2>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-10'>
+            <div className='flex gap-10 justify-start items-center'>
+              <p className='text-slate-500'>
+                Buy
+              </p>
+              <div>
+                <Progress value={80} color='#76FF7A' className='w-[300px] bg-slate-300' />
+              </div>
+              <p className='text-slate-500' >
+                76%
+              </p>
+            </div>
+            <div className='flex gap-10 justify-start items-center'>
+              <p className='text-slate-500' >
+                Hold
+              </p>
+              <div>
+                <Progress value={8} color='gray' className='w-[300px] bg-slate-300' />
+              </div>
+              <p className='text-slate-500' >
+                8%
+              </p>
+            </div>
+            <div className='flex gap-10 justify-start items-center'>
+              <p className='text-slate-500' >
+                Sell
+              </p>
+              <div>
+                <Progress value={16} color='red' className='w-[300px] bg-slate-300' />
+              </div>
+              <p className='text-slate-500' >
+                76%
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+    </>
   )
 }
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,42 +1,50 @@
+import { TrendingUp, Newspaper, BarChart } from 'lucide-react';
+
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'Crypto Taxes', label: 'Crypto Taxes' },
-    { href: '/', key: 'Free Tools', label: 'Free Tools' },
-    { href: '/', key: 'Resource Center', label: 'Resource Center' },
-  ];
-  
-  
-  // TABS SECTION
-  export const TABS = [
-    {
-      trigger: 'Overview',
-      description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
-    },
-    {
-      trigger: 'Fundamentals',
-      description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
-    },
-    {
-      trigger: 'New Insights',
-      description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
-    },
-    {
-      trigger: 'Sentiments',
-      description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
-    },
-    {
-      trigger: 'Team',
-      description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
-    },
-    {
-      trigger: 'Tokenomics',
-      description:
-        'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
-    },
-  ];
-  
+  { href: '/', key: 'Crypto Taxes', label: 'Crypto Taxes' },
+  { href: '/', key: 'Free Tools', label: 'Free Tools' },
+  { href: '/', key: 'Resource Center', label: 'Resource Center' },
+];
+
+
+export const SENTIMENT_ITEMS = [
+  { bg: 'bg-blue-700' },
+  {  bg: 'bg-green-700' },
+  { bg: 'bg-purple-700' },
+ ];
+
+
+// TABS SECTION
+export const TABS = [
+  {
+    trigger: 'Overview',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    trigger: 'Fundamentals',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    trigger: 'New Insights',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    trigger: 'Sentiments',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    trigger: 'Team',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+  {
+    trigger: 'Tokenomics',
+    description:
+      'We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location',
+  },
+];
