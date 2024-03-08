6cb234a9aa114f0a73991560842425f24b768f86
Vaibhav91one <vtking03@gmail.com>
2024-03-08 16:02:22 +0530

    fix: Actions URL fix

--- src/lib/actions/CoinGeckoActions.tsx
+++ src/lib/actions/CoinGeckoActions.tsx
@@ -3,16 +3,12 @@ import axios from "axios";
 export async function GetPrice() {
     const options = {
         method: 'GET',
-        url: 'https://coingecko.p.rapidapi.com/simple/price',
+        url: 'https://api.coingecko.com/api/v3/simple/price',
         params: {
             ids: 'bitcoin',
             vs_currencies: 'inr,usd',
             include_24hr_change: 'true'
         },
-        headers: {
-            'X-RapidAPI-Key': process.env.RAPID_GECKO_KEY,
-            'X-RapidAPI-Host': 'coingecko.p.rapidapi.com'
-        }
     };
 
     try {
